aed6e57d6fa4228075a537c111c7dc6fabd1a48a
Daniel Kim <daniel.kim1116@gmail.com>
2016-03-22 11:56:08 -0700

    fixed all name changes

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ var port = process.env.PORT || 3000;
 
 // Middleware Configuration 
 
-app.use(express.static(__dirname + '/client'));                 // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/../client'));                 // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
